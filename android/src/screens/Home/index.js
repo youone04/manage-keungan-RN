5d5d020e323dd5045fbe260ea9c8fd74b916bec8
youone04 <yudi.gunaone87@gmail.com>
2021-04-10 12:01:59 +0700

    hasil set in localstorage

--- android/src/screens/Home/index.js
+++ android/src/screens/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import {
     View , 
     Text ,
@@ -6,43 +6,94 @@ import {
     TextInput , 
     TouchableOpacity
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Home = ()=> {
-    const [tambah , setTambah] = useState(0);
-    const [kurang , setKurang] = useState(0);
-    const [hasil ,setHasil] = useState(0);
+class Home extends Component {
+    constructor(){
+        super();
+        this.state =  {
+            tambah : 0,
+            kurang : 0,
+            hasil: 0
+        }
+    }
+    async componentDidMount(){
+        // AsyncStorage.clear()
+        try{
+            const total = await AsyncStorage.getItem('total');
+            console.log('total', total);
+            if(total){
+                this.setState({
+                    hasil: parseInt(total)
+                })
+            }
+        }catch(e){
+            console.log(e)
+        }
 
-    return(
-        <>
-        <View style={styles.containerHeader}>
-           <View style={styles.contanerText}>
-            <Text style={styles.textClear}>clear</Text>
-            <Text style={styles.textRincian}>rinccian</Text>
-           </View>
-        </View>
+    }
+    
+    handleButton = async(data) =>{
+        const datas = await AsyncStorage.getItem('total');
+        console.log('data =>',datas)
+        const{hasil,tambah,kurang} = this.state;
+       if(data==='tambah'){
+        await this.setState({
+            hasil: hasil + parseInt(tambah),
+            tambah: 0
+        });
+              
+       }else if(data==='kurang'){
+        await this.setState({
+           hasil : hasil - kurang,
+           kurang: 0
+        });
+       }
+    }
+    async componentDidUpdate(){
+        const{hasil} =  this.state;
+        console.log('hasil =>',hasil);
+         try{
+           await  AsyncStorage.setItem('total',JSON.stringify(hasil))
+        }catch(e){
+            console.log(e)
+        }
+    }
 
-        <View style={styles.containerCardHasil}> 
-            <Text>hasil : {hasil}</Text>
-        </View>
-        <View>
-            <TextInput onChangeText={(text) => setTambah(text)} style={styles.textInput} placeholder="pemasukan"/>
-            <TouchableOpacity onPress={() =>  setHasil(hasil + parseInt(tambah))} style={styles.buttoPlus}>
-                <Text style={styles.textPlus}>Pemasukan</Text>
-            </TouchableOpacity>
-
-        </View>
-
-        <View>
-            <TextInput onChangeText={(text) => setKurang(text)} style={styles.textInput} placeholder="pemasukan"/>
-            <TouchableOpacity onPress={() =>  setHasil(hasil - kurang )} style={styles.buttoPlus}>
-                <Text style={styles.textPlus}>Pengeluaran</Text>
-            </TouchableOpacity>
-
-        </View>
-        
-
-        </>
-    )
+    render(){
+        const{hasil,tambah, kurang} = this.state;
+        return(
+            <>
+            <View style={styles.containerHeader}>
+               <View style={styles.contanerText}>
+                <Text style={styles.textClear}>clear</Text>
+                <Text style={styles.textRincian}>rinccian</Text>
+               </View>
+            </View>
+    
+            <View style={styles.containerCardHasil}> 
+                <Text>hasil : {hasil}</Text>
+            </View>
+            <View>
+                <TextInput value={tambah} onChangeText={(text) => this.setState({tambah:text})} style={styles.textInput} placeholder="pemasukan"/>
+                <TouchableOpacity onPress={() => this.handleButton('tambah')} style={styles.buttoPlus}>
+                    <Text style={styles.textPlus}>Pemasukan</Text>
+                </TouchableOpacity>
+    
+            </View>
+    
+            <View>
+                <TextInput value={kurang} onChangeText={(text) => this.setState({kurang:text})} style={styles.textInput} placeholder="pemasukan"/>
+                <TouchableOpacity onPress={() => this.handleButton('kurang') } style={styles.buttoPlus}>
+                    <Text style={styles.textPlus}>Pengeluaran</Text>
+                </TouchableOpacity>
+    
+            </View>
+            
+    
+            </>
+        )
+    }
 }
 
 export default Home;
